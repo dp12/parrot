449781d137b80a070264a33f41233c471937fefd
dp12 <deep.paren.12@gmail.com>
2019-10-15 17:21:22 -0400

    Update commentary

--- parrot.el
+++ parrot.el
@@ -22,9 +22,11 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
+;;
+;; A minor mode for displaying party parrot in the modeline and rotating words.
+;;
 ;; To load this file, add (require 'parrot) to your init file.  You can display
-;; the party parrot in your mode line by adding (parrot-mode).
+;; the party parrot in your modeline by adding (parrot-mode).
 ;;
 ;; To get the parrot to rotate on new email messages in mu4e, add:
 ;; (add-hook 'mu4e-index-updated-hook #'parrot-start-animation)
@@ -32,6 +34,9 @@
 ;; This animation code is a heavily modified version of Jacek "TeMPOraL"
 ;; Zlydach's famous nyan-mode.  Check out his original work at
 ;; https://github.com/TeMPOraL/nyan-mode/.
+;;
+;; Please see README.md for more documentation, or read it online at
+;; https://github.com/dp12/parrot
 
 ;;; Code:
 
