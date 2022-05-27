7bc47397d55d6ace04133c9e7e32230c32502ace
Psionik K <73710933+psionic-k@users.noreply.github.com>
2022-05-27 01:14:18 -0500

    Overhaul code structure, private
    Added an option, parrot-animate-on-load.
    
    Combined the frame list init code into one call.
    
    Deconflated parrot-animation-state and parrot-hide-if-not-animating
    
    Fixed a bug where I had regressed the handling of
    parrot--old-cdr-mode-line-position
    
    Added aliases where privates and moved variables will be missed by doom-modeline
    or people using old customize settings.
    
    Completely overhauled the order and naming to establish what is private to the
    implementation as it is and what is likely being used by users.
    
    Updated the `parrot--refresh` function to work with animations and initialize
    properly, after the entire feature is loaded so that setters on customize and
    elsewhere don't start calling things with half of package set up.

--- parrot.el
+++ parrot.el
@@ -49,17 +49,28 @@
 ;; ('v') (*'v') ('V'*) ('v'*)
 ;; ('v') ('V') ('>') ('^') ('<') ('V') ('v')
 
-(defgroup parrot nil
-  "Customization group for `parrot-mode'."
-  :group 'frames)
+;; Internal variables
+(defvar parrot--animation-timer nil
+  "Internal timer used for switching animation frames.")
+(defvar parrot--rotations 0
+  "Counter of how many times the parrot has rotated.")
+(defvar parrot--old-cdr-mode-line-position nil
+  "Used to store anything we clobber in the `mode-line-position'.")
+(defvar parrot--visible nil
+  "Controls `parrot--create' output.
+If you are a `doom-modeline' user, see `doom-modeline-segment--parrot'")
+(defvar parrot--animation-frames nil
+  "A list of the animation frames for the current parrot.")
+(defvar parrot--current-frame 0)
+(defvar parrot--frame-list (number-sequence 1 10)  ;; TODO this is a bit oddly named and also initialized wrong.
+  "List of indices for the parrot animation frames.
+For example, an animation with a total of ten frames would have a
+`parrot-frame-list` of (1 2 3 4 5 6 7 8 9 10)")
+(defvar parrot--static-image nil
+  "The image shown when parrot is at rest, i.e. not rotating.")
 
-(defun parrot-refresh ()
-  "Refresh after option change if loaded."
-  (when (featurep 'parrot)
-    (when (bound-and-true-p parrot-mode)
-      (force-mode-line-update))))
-
-(defun parrot-sequence-length (parrot)
+;; Internal functions
+(defun parrot--sequence-length (parrot)
   "Return length of the animation sequence for PARROT."
   (cond ((string= parrot "default") 10)
         ((string= parrot "confused") 26)
@@ -70,29 +81,40 @@
         ((string= parrot "thumbsup") 12)
         (t (error (format "Invalid parrot %s" parrot)))))
 
-(defcustom parrot-animation-frame-interval 0.045
-  "Number of seconds between animation frames."
-  :group 'parrot
-  :type 'float
-  :set (lambda (sym val)
-         (set-default sym val)
-         (parrot-refresh)))
+(defun parrot--create-frame (parrot id)
+  "Create image for frame with parrot type PARROT and frame id ID."
+  (create-image (concat parrot-directory
+                        (format "img/%s/%s-parrot-frame-%d.xpm" parrot parrot id)) 'xpm nil :ascent 'center))
 
-(defcustom parrot-click-hook nil
-  "Hook run after clicking on the parrot."
-  :group 'parrot
-  :type 'hook)
+(defun parrot--load-frames (parrot)
+  "Load the images for the selected PARROT."
+  (setq parrot--frame-list (number-sequence 1 (parrot--sequence-length parrot)))
+  (when (image-type-available-p 'xpm)
+    (setq parrot--static-image (parrot--create-frame parrot 1))
+    (setq parrot--animation-frames (mapcar (lambda (id)
+                                            (parrot--create-frame parrot id))
+                                          parrot--frame-list))))
 
-(defvar parrot-animation-timer nil
-  "Internal timer used for switching animation frames.")
+(defun parrot--refresh (&optional maybe-static)
+  "Show the results of updated options.
 
-(defvar parrot-rotations 0
-  "Counter of how many times the parrot has rotated.")
+MAYBE-STATIC is appropriate for changes that do not affect
+animation and cannot be better demonstrated with animation.  This
+is also affected by `parrot-animate'.  If the parrot only shows
+during animation, then it will animate to show the update.
 
-(defvar parrot-old-cdr-mode-line-position nil)
-(defvar parrot--visible nil
-  "Controls `parrot-create' output.
-If you are a `doom-modeline' user, see `doom-modeline-segment--parrot'")
+This refresh will enable the mode if necessary to provide user
+feedback, which could also animate depending on settings."
+  (when (featurep 'parrot)
+    (if (not parrot-mode)
+        (parrot-mode 1)
+      (parrot--load-frames parrot-type))
+    (if (and parrot-animate
+             (not
+              (or (eq parrot-animate 'no-animation)
+                  maybe-static)))
+        (parrot-start-animation)
+      (force-mode-line-update))))
 
 (defun parrot--show-parrot ()
   "Add parrot to the modeline.
@@ -101,10 +123,10 @@ If you are a `doom-modeline' user, see
 using `parrot-create' directly whenever `parrot-mode' is active."
   (unless parrot--visible
     (progn
-      (unless parrot-old-cdr-mode-line-position
-        (setq parrot-old-cdr-mode-line-position (cdr mode-line-position))
-        (setcdr mode-line-position (cons '(:eval (list (parrot-create)))
-                                         (cdr parrot-old-cdr-mode-line-position))))
+      (unless parrot--old-cdr-mode-line-position
+        (setq parrot--old-cdr-mode-line-position (cdr mode-line-position))
+        (setcdr mode-line-position (cons '(:eval (list (parrot--create)))
+                                         (cdr parrot--old-cdr-mode-line-position))))
       (setf parrot--visible t)
       (force-mode-line-update))))
 
@@ -112,53 +134,70 @@ using `parrot-create' directly whenever `parrot-mode' is active."
   "Remove parrot from modeline."
   (when parrot--visible
     (progn
-      (setcdr mode-line-position nil)
-      (setf parrot-old-cdr-mode-line-position nil)
+      (setcdr mode-line-position parrot--old-cdr-mode-line-position)
+      (setf parrot--old-cdr-mode-line-position nil)
       (setf parrot--visible nil)
       (force-mode-line-update))))
 
-(defun parrot-start-animation (&optional persist)
-  "Start the parrot animation.
-PERSIST will set `parrot-rotations' to -1 and cause infinite
-animation until `parrot-stop-animation' is called."
-  (interactive)
-  (when (not (bound-and-true-p parrot-mode))
-    (parrot-mode))
-  (parrot--show-parrot)
-  (unless (eq parrot-rotations -1)
-    (setq parrot-rotations (if persist -1 0)))
-  (when (not (and parrot-animate-parrot
-                  parrot-animation-timer))
-    (setq parrot-animation-timer (run-at-time nil
-                                              parrot-animation-frame-interval
-                                              #'parrot-switch-anim-frame))
-    (setq parrot-animate-parrot t)))
+(defun parrot--switch-anim-frame ()
+  "Change to the next frame in the parrot animation.
+If the parrot has already rotated for `parrot-num-rotations', the animation will
+stop."
+  (setq parrot--current-frame (% (+ 1 parrot--current-frame) (car (last parrot--frame-list))))
+  (when (eq parrot--current-frame 0)
+    (unless (eq -1 parrot--rotations)
+      (setq parrot--rotations (+ 1 parrot--rotations)))
+    (when (and parrot-num-rotations (>= parrot--rotations parrot-num-rotations))
+      (parrot-stop-animation)))
+  (force-mode-line-update))
 
-(defcustom parrot-hide-when-not-animating nil
-  "If non-nil, parrot will be hidden when not animating."
+(defun parrot--get-anim-frame ()
+  "Get the current animation frame."
+  (if (not (memq parrot-animate '(no-animation)))
+      (nth parrot--current-frame parrot--animation-frames)
+    parrot--static-image))
+
+(defun parrot--add-click-handler (string)
+  "Add a handler to STRING for animating the parrot when it is clicked."
+  (propertize string 'keymap `(keymap (mode-line keymap (down-mouse-1 . ,(lambda () (interactive)
+                                                                           (parrot-start-animation)
+                                                                           (run-hooks 'parrot-click-hook)))))))
+
+(defun parrot--create ()
+  "Generate the party parrot string."
+  (if (or (not parrot--visible)
+          (< (window-width) parrot-minimum-window-width))
+      ""                                ; disabled for too small windows
+    (let ((parrot-string (make-string parrot-spaces-before ?\s)))
+      (setq parrot-string (concat parrot-string (parrot--add-click-handler
+                                                 (propertize "-" 'display (parrot--get-anim-frame)))
+                                  (make-string parrot-spaces-after ?\s)))
+      (propertize parrot-string 'help-echo parrot-modeline-help-string))))
+
+(defalias 'parrot-create 'parrot--create
+  "For `doom-modeline-segment--parrot' and other custom modelines
+that want to control the position of the parrot.")
+
+;; customize group
+(defgroup parrot nil
+  "Customization group for `parrot-mode'."
+  :group 'frames)
+
+(defcustom parrot-animation-frame-interval 0.045
+  "Number of seconds between animation frames."
   :group 'parrot
-  :type 'boolean
+  :type 'float
   :set (lambda (sym val)
          (set-default sym val)
-         (when (bound-and-true-p parrot-mode)
-             (if  val (parrot--show-parrot)
-               (parrot--remove-parrot)))))
+         (parrot--refresh)))
 
-(defun parrot-stop-animation ()
-  "Stop the parrot animation.
-If a persistent animation is being broken, animation will
-continue for `parrot-num-roatiations'"
-  (interactive)
-  (if (eq parrot-rotations -1)
-      (setq parrot-rotations 0)
-    (when (and parrot-animate-parrot
-               parrot-animation-timer)
-      (cancel-timer parrot-animation-timer)
-      (setq parrot-animation-timer nil)
-      (setq parrot-animate-parrot nil)  ;; TODO redundant state
-      (setq parrot-rotations 0))
-    (when parrot-hide-when-not-animating
-      (parrot--remove-parrot))))
+(defcustom parrot-click-hook nil
+  "Hook run after clicking on the parrot."
+  :group 'parrot
+  :type 'hook
+  :set (lambda (sym val)
+         (set-default sym val)
+         (parrot--refresh t)))
 
 (defcustom parrot-minimum-window-width 45
   "Determines the minimum width of the window, below which party parrot will not be displayed."
@@ -166,19 +205,7 @@ continue for `parrot-num-roatiations'"
   :type 'integer
   :set (lambda (sym val)
          (set-default sym val)
-         (parrot-refresh)))
-
-(defcustom parrot-animate-parrot nil
-  "If non-nil, parrot animation is enabled."
-  :group 'parrot
-  :type '(choice (const :tag "Enabled" t)
-                 (const :tag "Disabled" nil))
-  :set (lambda (sym val)
-         (set-default sym val)
-         (if val
-             (parrot-start-animation)
-           (parrot-stop-animation))
-         (parrot-refresh)))
+         (parrot--refresh t)))
 
 (defcustom parrot-spaces-before 0
   "Spaces of padding before parrot in mode line."
@@ -186,7 +213,7 @@ continue for `parrot-num-roatiations'"
   :type 'integer
   :set (lambda (sym val)
          (set-default sym val)
-         (parrot-refresh)))
+         (parrot--refresh t)))
 
 (defcustom parrot-spaces-after 0
   "Spaces of padding after parrot in the mode line."
@@ -194,27 +221,44 @@ continue for `parrot-num-roatiations'"
   :group 'parrot
   :set (lambda (sym val)
          (set-default sym val)
-         (parrot-refresh)))
+         (parrot--refresh t)))
 
 (defcustom parrot-num-rotations 3
   "How many times party parrot will rotate."
   :group 'parrot
-  :type 'integer)
+  :type 'integer
+  :set (lambda (sym val)
+         (set-default sym val)
+         (parrot--refresh)))
 
-(defun parrot-create-frame (parrot id)
-  "Create image for frame with parrot type PARROT and frame id ID."
-  (create-image (concat parrot-directory
-                        (format "img/%s/%s-parrot-frame-%d.xpm" parrot parrot id)) 'xpm nil :ascent 'center))
+(define-obsolete-variable-alias 'parrot-animate-parrot 'parrot-animate)
+(defcustom parrot-animate t
+  "Animation and show/hide preference.
+Possible values: non-nil to always show the parrot, animating or
+not.  `'no-animation' to show the parrot but never animate
+it.  `'hide-static' to only show the parrot when not animating.
 
-(defun parrot-load-frames (parrot)
-  "Load the images for the selected PARROT."
-  (when (image-type-available-p 'xpm)
-    (setq parrot-static-image (parrot-create-frame parrot 1))
-    (setq parrot-animation-frames (mapcar (lambda (id)
-                                            (parrot-create-frame parrot id))
-                                          parrot-frame-list))))
+nil will rever to the legacy `parrot-animate-parrot' no animation
+behavior, the same as `'no-animation'."
+  :group 'parrot
+  :type '(choice (const :tag "Always show" t)
+                 (const :tag "No animation" 'no-animation)
+                 (const :tag "Hide when not animating" 'hide-static))
+  :set (lambda (sym val)
+         ;; map legacy nil value to 'no-animation
+         (let ((val (if (eq val nil) 'no-animation val)))
+           (set-default sym val)
+           (parrot--refresh))))
 
-(defcustom parrot-type 'default
+(defcustom parrot-animate-on-load t
+  "If non-nil animate when `parrot-mode' enabled."
+  :group 'parrot
+  :type 'boolean
+  :set (lambda (sym val)
+               (set-default sym val)
+               (parrot--refresh)))
+
+(defcustom parrot-type 'default ;; TODO get initialization coordinated with mode
   "What kind of parrot, such as default or nyan.
 Also see `parrot-set-parrot-type'."
   :group 'parrot
@@ -227,76 +271,80 @@ Also see `parrot-set-parrot-type'."
                  (const :tag "Thumbsup" thumbsup))
   :set (lambda (sym val)
          (set-default sym val)
-         (setq parrot-frame-list (number-sequence 1 (parrot-sequence-length val)))
-         (parrot-load-frames val)
-         (run-at-time "0.5 seconds" nil #'parrot-start-animation)
+         (parrot--refresh)
          (message (format "%s parrot selected" val))))
 
-(defvar parrot-frame-list (number-sequence 1 10)
-  "List of indices for the parrot animation frames.
-For example, an animation with a total of ten frames would have a
-`parrot-frame-list` of (1 2 3 4 5 6 7 8 9 10)")
+;; user commands
 
-(defvar parrot-static-image nil
-  "The image shown when parrot is at rest, i.e. not rotating.")
+(defun parrot-start-animation (&optional persist force-animation)
+  "Start the parrot animation.
+PERSIST will set `parrot--rotations' to -1 and cause infinite
+animation until `parrot-stop-animation' is called.
 
-(defvar parrot-animation-frames nil
-  "A list of the animation frames for the current parrot.")
+FORCE-ANIMATION is a non-interactive way to ignore `parrot-animate'
+
+When called interactively, this function does not respect
+`parrot-animate' and will always animate."
+  (interactive)
+  (let* ((user (called-interactively-p 'interactive))
+         (animate (or user
+                      (not (eq parrot-animate 'no-animation))
+                      force-animation)))
+
+    ;; In any case where loading the mode starts an animation, this call is
+    ;; idempotent with the mode
+    (when (not (bound-and-true-p parrot-mode))
+      (parrot-mode))
+    (parrot--show-parrot) ;; also idempotent over `parrot--visible'
+    (unless (eq parrot--rotations -1)
+      (setq parrot--rotations (if persist -1 0)))
+    (when animate
+      (when (not parrot--animation-timer)
+        (setq parrot--animation-timer (run-at-time nil
+                                                   parrot-animation-frame-interval
+                                                   #'parrot--switch-anim-frame))))))
+
+(defun parrot-stop-animation ()
+  "Stop the parrot animation.
+If a persistent animation is being broken, animation will
+continue for `parrot-num-roatiations'"
+  (interactive)
+  (if (eq parrot--rotations -1)
+      (setq parrot--rotations 0)
+    (when parrot--animation-timer
+      (cancel-timer parrot--animation-timer)
+      (setq parrot--animation-timer nil)
+      (setq parrot--rotations 0))
+    (unless (memq parrot-animate '(t 'no-animation))
+      (parrot--remove-parrot))))
 
 (defun parrot-set-parrot-type (parrot &optional silent)
   "Set the desired PARROT type in the mode line.
 SILENT will not show the parrot even if settings enable it."
-  (interactive (list
-                (completing-read "Select parrot: "
-                                 '(default confused emacs nyan rotating science thumbsup) nil t)))
-  (custom-set-variables `(parrot-type ,parrot)))
-
-(defvar parrot-current-frame 0)
-
-(defun parrot-switch-anim-frame ()
-  "Change to the next frame in the parrot animation.
-If the parrot has already rotated for `parrot-num-rotations', the animation will
-stop."
-  (setq parrot-current-frame (% (+ 1 parrot-current-frame) (car (last parrot-frame-list))))
-  (when (eq parrot-current-frame 0)
-    (unless (eq -1 parrot-rotations)
-      (setq parrot-rotations (+ 1 parrot-rotations)))
-    (when (and parrot-num-rotations (>= parrot-rotations parrot-num-rotations))
-      (parrot-stop-animation)))
-  (force-mode-line-update))
-
-(defun parrot-get-anim-frame ()
-  "Get the current animation frame."
-  (if parrot-animate-parrot
-      (nth parrot-current-frame parrot-animation-frames)
-    parrot-static-image))
-
-(defun parrot-add-click-handler (string)
-  "Add a handler to STRING for animating the parrot when it is clicked."
-  (propertize string 'keymap `(keymap (mode-line keymap (down-mouse-1 . ,(lambda () (interactive)
-                                                                           (parrot-start-animation)
-                                                                           (run-hooks 'parrot-click-hook)))))))
-(defun parrot-create ()
-  "Generate the party parrot string."
-  (if (or (not parrot--visible)
-          (< (window-width) parrot-minimum-window-width))
-      ""                                ; disabled for too small windows
-    (let ((parrot-string (make-string parrot-spaces-before ?\s)))
-      (setq parrot-string (concat parrot-string (parrot-add-click-handler
-                                                 (propertize "-" 'display (parrot-get-anim-frame)))
-                                  (make-string parrot-spaces-after ?\s)))
-      (propertize parrot-string 'help-echo parrot-modeline-help-string))))
+  (interactive
+   (list
+    (completing-read "Select parrot: "
+                     ;; TODO use the customize options list
+                     '(default confused emacs nyan rotating science thumbsup) nil t)))
+  (if silent
+      (prog1
+          (setq parrot-type parrot)
+        (parrot--load-frames parrot-type))
+    (custom-set-variables `(parrot-type ,parrot))))
 
 ;;;###autoload
 (define-minor-mode parrot-mode
   "Use Parrot to show when you're rotating.
-You can customize this minor mode, see option `parrot-mode'."
+You can customize this minor mode, see `customize-group' `parrot'."
   :global t
   :require 'parrot
   (if parrot-mode
       (progn
-        (unless parrot-hide-when-not-animating
-          (parrot--show-parrot))
+        (parrot--load-frames parrot-type)
+        (if parrot-animate-on-load
+            (parrot-start-animation nil t)
+          (if (memq parrot-animate '(t no-animation))
+            (parrot--show-parrot)))
         (parrot--maybe-add-todo-hook)
         (parrot--maybe-advise-magit-push))
     (progn
